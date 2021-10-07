b1004d5b430b91231a24a2fb70567d9ed97eab52
Arc-blroth <45273859+Arc-blroth@users.noreply.github.com>
2021-10-06 19:38:07 -0700

    implement the remote side of Rooster
    Signed-off-by: Arc-blroth <45273859+Arc-blroth@users.noreply.github.com>

--- hammer/hammeregg_rooster/src/main.rs
+++ hammer/hammeregg_rooster/src/main.rs
@@ -17,9 +17,50 @@ use tokio::net::{TcpListener, TcpStream};
 use tokio_tungstenite::tungstenite::Message;
 use tokio_tungstenite::WebSocketStream;
 
+/// A wrapper around a desktop and a set of peers
+/// where each peer has a unique id.
+pub struct DesktopAndPeers {
+    pub desktop_tx: UnboundedSender<Message>,
+    id_counter: u32,
+    peers: HashMap<u32, UnboundedSender<Message>>,
+}
+
+impl DesktopAndPeers {
+    pub fn new(desktop_tx: UnboundedSender<Message>) -> Self {
+        Self {
+            desktop_tx,
+            id_counter: 0,
+            peers: HashMap::new(),
+        }
+    }
+
+    pub fn insert_peer(&mut self, peer: UnboundedSender<Message>) -> u32 {
+        let id = self.id_counter;
+        self.peers.insert(id, peer);
+        self.id_counter += 1;
+        id
+    }
+
+    pub fn get_peer(&self, id: &u32) -> Option<&UnboundedSender<Message>> {
+        self.peers.get(id)
+    }
+
+    pub fn get_peer_mut(&mut self, id: &u32) -> Option<&mut UnboundedSender<Message>> {
+        self.peers.get_mut(id)
+    }
+
+    pub fn remove_peer(&mut self, id: &u32) {
+        self.peers.remove(id);
+    }
+
+    pub fn peers(&self) -> &HashMap<u32, UnboundedSender<Message>> {
+        &self.peers
+    }
+}
+
 /// A concurrent map between home desktops' names and a tuple of
 /// their sending end and their peers' sending ends.
-type Desktops = Arc<Mutex<HashMap<String, (UnboundedSender<Message>, HashMap<u32, UnboundedSender<Message>>)>>>;
+type Desktops = Arc<Mutex<HashMap<String, DesktopAndPeers>>>;
 
 #[tokio::main]
 async fn main() -> Result<()> {
@@ -76,6 +117,9 @@ async fn handle_connection(desktops: Desktops, stream: TcpStream) {
             HandshakePacket::HomeInit { home_name } => {
                 handle_home_init(desktops, socket, home_name).await?;
             }
+            HandshakePacket::RemoteInit { home_name } => {
+                handle_remote_init(desktops, socket, home_name).await?;
+            }
             _ => Err(anyhow!(
                 "Handshake failed: client did not send a valid HandshakeInitPacket"
             ))?,
@@ -104,7 +148,7 @@ async fn handle_home_init(desktops: Desktops, mut socket: WebSocketStream<TcpStr
 
         let (tx, rx) = unbounded();
         // Insert sender into desktop map
-        desktops.lock().insert(home_name.clone(), (tx, HashMap::new()));
+        desktops.lock().insert(home_name.clone(), DesktopAndPeers::new(tx));
 
         let (send, recv) = socket.split();
 
@@ -120,13 +164,9 @@ async fn handle_home_init(desktops: Desktops, mut socket: WebSocketStream<TcpStr
                         HandshakePacket::HomeAnswerSuccess { peer, .. }
                         | HandshakePacket::HomeAnswerFailure { peer, .. } => {
                             let mut desktop_map = desktops.lock();
-                            let peers = &mut desktop_map.get_mut(&home_name).unwrap().1;
-                            if peers.contains_key(&peer) {
-                                peers
-                                    .get_mut(&peer)
-                                    .unwrap()
-                                    .unbounded_send(packet)
-                                    .context("Couldn't send packet")?
+                            let maybe_peer = desktop_map.get_mut(&home_name).unwrap().get_peer_mut(&peer);
+                            if let Some(peer) = maybe_peer {
+                                peer.unbounded_send(packet).context("Couldn't send packet")?
                             } else {
                                 // Whoops the peer no longer exists
                                 Err(anyhow!("Signalling failed: peer does not exist (any longer)"))?
@@ -146,7 +186,85 @@ async fn handle_home_init(desktops: Desktops, mut socket: WebSocketStream<TcpStr
         future::select(send_home, send_peer).await;
 
         // Disconnect
-        desktops.lock().remove(&home_name);
+        let mut desktop_map = desktops.lock();
+        if let Some(desktop) = desktop_map.remove(&home_name) {
+            desktop.peers().iter().for_each(|(_, peer)| {
+                peer.close_channel();
+            });
+        }
+    }
+    Ok(())
+}
+
+async fn handle_remote_init(
+    desktops: Desktops,
+    mut socket: WebSocketStream<TcpStream>,
+    home_name: String,
+) -> Result<()> {
+    if !desktops.lock().contains_key(&home_name) {
+        // oops desktop does not exist
+        socket
+            .send(serialize_packet(&HandshakePacket::RemoteInitResponse {
+                response: Err(ErrorMsg("Requested desktop not found".to_string())),
+            })?)
+            .await?;
+    } else {
+        // Initial handshake complete!
+        socket
+            .send(serialize_packet(&HandshakePacket::RemoteInitResponse {
+                response: Ok(()),
+            })?)
+            .await?;
+
+        let (tx, rx) = unbounded();
+        // Insert sender into desktop map
+        let id = desktops
+            .lock()
+            .get_mut(&home_name)
+            .context("Desktop disappeared during remote init?")?
+            .insert_peer(tx);
+
+        let (send, recv) = socket.split();
+
+        // Listen to incoming requests to send back to the remote
+        let send_remote = rx.map(Ok).forward(send);
+
+        // Listen to incoming requests to send home
+        let send_home = recv
+            .map(|res| res.context("Signalling failed: could not read packet"))
+            .try_for_each(|packet| {
+                match try {
+                    match deserialize_packet::<HandshakePacket>(&packet)? {
+                        HandshakePacket::RemoteOffer { payload, .. } => {
+                            let mut desktop_map = desktops.lock();
+                            let desktop = desktop_map
+                                .get_mut(&home_name)
+                                .context("Desktop does not exist any longer")?;
+
+                            // Since remote doesn't know their peer id we need to fill it in
+                            let filled_packet = serialize_packet(&HandshakePacket::RemoteOffer { peer: id, payload })?;
+
+                            desktop
+                                .desktop_tx
+                                .unbounded_send(filled_packet)
+                                .context("Couldn't send packet")?;
+                        }
+                        _ => Err(anyhow!("Signalling failed: did not get a RemoteOffer packet"))?,
+                    }
+                } {
+                    Ok(_) => future::ok(()),
+                    Err(err) => future::err(err),
+                }
+            });
+
+        pin_mut!(send_remote, send_home);
+        future::select(send_remote, send_home).await;
+
+        // Disconnect
+        let mut desktop_map = desktops.lock();
+        if let Some(desktop) = desktop_map.get_mut(&home_name) {
+            desktop.remove_peer(&id);
+        }
     }
     Ok(())
 }
